848015cfc08d5c4ad148fbcfa6ec02f33562087d
Ph.Minh.Dong <142565609+dongdev06@users.noreply.github.com>
2023-10-18 20:16:00 +0700

    Update broadcast.js

--- broadcast.js
+++ broadcast.js
@@ -7,7 +7,7 @@ switch (global.Fca.Require.FastConfig.BroadCast) {
                 var Fetch = global.Fca.Require.Fetch;
                     Fetch.get("https://raw.githubusercontent.com/KanzuXHorizon/Global_Horizon/main/Fca_BroadCast.json").then(async (/** @type {{ body: { toString: () => string; }; }} */ res) => {
                         global.Fca.Data.BroadCast = JSON.parse(res.body.toString())
-                    var random = JSON.parse(res.body.toString())[Math.floor(Math.random() * JSON.parse(res.body.toString()).length)] || "Ae Zui Zẻ Nhé !";
+                    var random = JSON.parse(res.body.toString())[Math.floor(Math.random() * JSON.parse(res.body.toString()).length)] || "Ae Zui Zẻ Nhé!";
                 logger.Normal(random);
             }); 
         }	
@@ -18,7 +18,7 @@ switch (global.Fca.Require.FastConfig.BroadCast) {
             try {
                 try {
                     var logger = global.Fca.Require.logger;
-                        var random = global.Fca.Data.BroadCast[Math.floor(Math.random() * global.Fca.Data.BroadCast.length)] || "Ae Zui Zẻ Nhé !";
+                        var random = global.Fca.Data.BroadCast[Math.floor(Math.random() * global.Fca.Data.BroadCast.length)] || "Ae Zui Zẻ Nhé!";
                     logger.Normal(random);
                 }	
                 catch (e) {
@@ -29,7 +29,7 @@ switch (global.Fca.Require.FastConfig.BroadCast) {
             catch (e) {
                 console.log(e);
             }
-        },3600 * 1000);
+        }, 3600 * 1000);
     }
     case false: {
         break;
